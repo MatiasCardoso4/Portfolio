57db2ab902783148a9a51f5a0293e18a6e328948
MatiasCardoso4 <cardosomatias1994@gmail.com>
2025-02-22 11:39:48 -0300

    add social media icons and global styles for portfolio

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-var hamburguer = document.querySelector('.hamburguer');
-var navMenu = document.querySelector('.nav-menu');
-console.log(navMenu);
-hamburguer === null || hamburguer === void 0 ? void 0 : hamburguer.addEventListener('click', function () {
+"use strict";
+const hamburguer = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.nav-menu');
+const openSocial = document.querySelector('.open');
+const social = document.querySelector('.social');
+hamburguer === null || hamburguer === void 0 ? void 0 : hamburguer.addEventListener('click', () => {
     hamburguer === null || hamburguer === void 0 ? void 0 : hamburguer.classList.toggle('dark');
     navMenu === null || navMenu === void 0 ? void 0 : navMenu.classList.toggle('active');
+});
+openSocial === null || openSocial === void 0 ? void 0 : openSocial.addEventListener('click', () => {
+    social === null || social === void 0 ? void 0 : social.classList.toggle('open-social');
 });
--- script.ts
+++ script.ts
@@ -1,9 +1,14 @@
 const hamburguer = document.querySelector('.hamburguer')
 const navMenu = document.querySelector('.nav-menu')
 
-console.log(navMenu);
+const openSocial = document.querySelector('.open')
+const social = document.querySelector('.social')
 
 hamburguer?.addEventListener('click', () => {
     hamburguer?.classList.toggle('dark')
     navMenu?.classList.toggle('active')
+})
+
+openSocial?.addEventListener('click', () => {
+    social?.classList.toggle('open-social')
 })
